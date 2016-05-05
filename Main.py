3325830d2513c48b5c4e8f947fa23b269f87b055
Retiwer <carlosbaez2@hotmail.com>
2016-05-05 14:22:52 -0400

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,8 @@
+import Libreria
+
+################	   Excel	     ################
+wb = openpyxl.load_workbook('/Test/Base.xlsx')
+sheet = wb.get_sheet_by_name('Hoja1')
+#####################################################
+
+Procesamiento(2,4985,2,21,1)
