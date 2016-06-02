59f1d3f75e9e6a906faae296f8f121f5d683378b
Retiwer <carlosbaez2@hotmail.com>
2016-06-02 15:53:27 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,20 @@
-import Libreria
+import Libreria  #Importar la libreria creada
+
+"""
+La Tabla de Excel fue copiada en una hoja diferente de la que originalmente se
+presento, aun dentro del mismo documento, debido a que existian problemas de 
+formatoen la hoja de excel, externos a los configurados o soportados por la
+funcion de lectura.
+los datos en la nueva hoja no fueron alterados en ningun otro aspecto mas que
+la ubicacion y el orden de las columnas.  la informacion numerica se mantiene
+identica a la original.
+"""
+
 
 ################	   Excel	     ################
-wb = openpyxl.load_workbook('/Test/Base.xlsx')
-sheet = wb.get_sheet_by_name('Hoja1')
+wb = openpyxl.load_workbook('/Test/Base.xlsx')  #Abrir el archivo Excel
+sheet = wb.get_sheet_by_name('Hoja1')           #Identificar la hoja de Datos
 #####################################################
 
-Procesamiento(2,4985,2,21,1)
+Procesamiento(2, 4985, 5, 24, 4)  #Llamar la funcion principal con los
+                                  #parametros correctos
